9055d2048613e72b4039b14b89ff4144b9d6abbd
pyTimK <timkristianllanto.tk@gmail.com>
2025-08-19 01:04:37 +0800

    .

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ TITLE = "Slash And Burn"
 def main():
     subprocess.run(
         # --noconsole
-        rf'pyinstaller --noconsole -F -w --add-data ".env;." --add-data="assets\\*.png;assets" --add-data="*.pt;." --add-data="credentials.json;." --icon=assets\\pyinstaller.ico app.py',
+        rf'pyinstaller -F -w --add-data ".env;." --add-data="assets\\*.png;assets" --add-data="*.pt;." --add-data="credentials.json;." --icon=assets\\pyinstaller.ico app.py',
         shell=True,
     )
     FileHelper.move("dist/app.exe", f"{TITLE}.exe")
